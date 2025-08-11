1cd6cd4635dddc67d87ca7703860b8051eec2990
Angel Serra <angeldavidserraperales@gmail.com>
2025-08-10 18:00:41 -0700

    Se le añadió una meta, y al llegar a ella te regresa al principio par volver a jugar el juego

--- project_gameADSP/meta.gd
+++ project_gameADSP/meta.gd
@@ -1,15 +1,23 @@
 extends Sprite2D
 
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
 
-# Called when the node enters the scene tree for the first time.
+@export_file("*.tscn") var escena_cargar: String
+
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	pass
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print ("llegaste a la meta")
+	
+	collision_shape_2d.set_deferred("disabled", true)
+	
+	if escena_cargar:
+		pass
+		get_tree().change_scene_to_file(escena_cargar)
